e9599049cddfd88dd27d0c303fa3dc68a98b257e
Nuno Carvalho <nuno@mygateway.xyz>
2025-01-15 16:42:04 +0000

    feat: add Gateway Shield Testnet configuration to wagmi setup

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,13 +1,28 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { sepolia, mainnet } from "wagmi/chains";
-import { http } from "wagmi";
+import { Chain } from "wagmi/chains";
+
+export const shield = {
+  id: 678746,
+  rpcUrls: {
+    default: { http: ["https://gateway-shield-testnet.rpc.caldera.xyz/http"] },
+  },
+  name: "Gateway Shield Testnet",
+  nativeCurrency: {
+    name: "Gateway",
+    symbol: "OWN",
+    decimals: 18,
+  },
+  blockExplorers: {
+    default: {
+      name: "Gateway Shield Explorer",
+      url: "https://gateway-shield-testnet.explorer.caldera.xyz/",
+    },
+  },
+} as const satisfies Chain;
 
 export const config = getDefaultConfig({
   appName: "Crypto Rock Paper Scissors",
   projectId: "YOUR_PROJECT_ID",
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
+  chains: [shield],
+  ssr: true,
 });
