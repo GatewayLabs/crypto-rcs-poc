c0bb389df9d68fcf633f77add7e375341720ec73
João Conserva <boscoj@outlook.com>
2025-02-26 18:51:18 -0300

    feat: Display user balance in game header component

--- components/game/game-header.tsx
+++ components/game/game-header.tsx
@@ -1,10 +1,23 @@
 "use client";
 
-import { useGameUIStore } from "@/stores/game-ui-store";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { formatEther } from "ethers";
+import { useEffect, useState } from "react";
+import { useAccount, useBalance } from "wagmi";
 
 export default function Header() {
-  const { playerRank } = useGameUIStore();
+  const { address, isConnected } = useAccount();
+  const { data: userBalance, isLoading } = useBalance({
+    address,
+  });
+  const [balance, setBalance] = useState<bigint | undefined>(BigInt(0));
+
+  useEffect(() => {
+    if (!isConnected || isLoading) {
+      return;
+    }
+    setBalance(userBalance?.value);
+  }, [isConnected, userBalance, isLoading]);
 
   return (
     <div className="bg-white flex w-full items-center justify-between flex-wrap rounded-3xl max-md:max-w-full">
@@ -18,11 +31,9 @@ export default function Header() {
       </div>
       <div className="bg-zinc-950 border-b border-l self-stretch flex min-w-60 min-h-[95px] flex-col overflow-hidden justify-center flex-1 shrink basis-[0%] my-auto px-6 py-7 rounded-3xl border-white border-solid max-md:max-w-full max-md:px-5 items-end">
         <div className="flex items-center gap-2 text-neutral-50 font-normal">
-          <div className="bg-[rgba(141,12,255,1)] overflow-hidden text-xs whitespace-nowrap leading-none p-3 rounded-sm">
-            Rank #{playerRank}
-          </div>
-          <div className="bg-zinc-800 overflow-hidden text-sm whitespace-nowrap leading-none my-auto p-3 rounded-sm">
-            10xp
+          <div className="bg-zinc-800 border-zinc-800 border self-stretch flex min-w-16 items-center overflow-hidden justify-center my-auto opacity-80 px-4 py-1.5 rounded-md border-solid transition-all duration-300 group-hover:opacity-50 hover:!opacity-100">
+            {balance ? parseFloat(formatEther(balance)).toFixed(4) : "0.0000"}{" "}
+            MON
           </div>
           <ConnectButton showBalance={false} />
         </div>
