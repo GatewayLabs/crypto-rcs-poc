9f2fe1329ffa1c27845f8379e37dd75a64c3a170
João Conserva <boscoj@outlook.com>
2025-02-28 16:15:47 -0300

    feat: Update footer component to enhance branding presentation with a powered by section

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,13 +3,15 @@
 export default function Footer() {
   return (
     <footer className="flex w-full md:items-center justify-between text-[11px] text-white font-normal md:text-center max-md:max-w-full pt-12 relative max-md:flex-col max-md:gap-4">
-      <img
-        loading="lazy"
-        src="gateway-logo.svg"
-        className="object-contain self-stretch my-auto max-md:w-[100px]"
-        alt="Left Logo"
-      />
-      <p>Built on the first L1 to unify public and private state</p>
+      <div className="flex gap-2 items-center">
+        Powered by
+        <img
+          loading="lazy"
+          src="gateway-logo.svg"
+          className="object-contain self-stretch my-auto max-md:w-[100px] ml-1"
+          alt="Left Logo"
+        />
+      </div>
       <img
         loading="lazy"
         src="icons/footer-img.svg"
