6868f9dbea15f9f867cb388c637919342b70262c
Nuno Carvalho <nunoormaker21@gmail.com>
2025-03-03 18:46:09 +0000

    chore: Update Monad RPC URL to use Alchemy API key

--- config/chains.ts
+++ config/chains.ts
@@ -3,7 +3,11 @@ import { Chain } from 'wagmi/chains';
 export const monad = {
   id: 10143,
   rpcUrls: {
-    default: { http: ['https://testnet-rpc.monad.xyz/'] },
+    default: {
+      http: [
+        `https://monad-testnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+      ],
+    },
   },
   name: 'Monad Testnet',
   nativeCurrency: {
