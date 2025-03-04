210e228ed11ab289b9d1e66c25d98f9de0cec4b4
Nuno Carvalho <nunoormaker21@gmail.com>
2025-03-04 23:47:16 +0000

    refactor: Use environment variable for subgraph URL

--- hooks/use-leaderboard.ts
+++ hooks/use-leaderboard.ts
@@ -3,8 +3,7 @@ import { LeaderboardEntry } from '@/types/game';
 import { formatEther } from 'viem';
 
 // Subgraph URL
-const SUBGRAPH_URL =
-  'https://api.studio.thegraph.com/query/105896/odyssey-rps/version/latest';
+const SUBGRAPH_URL = process.env.NEXT_PUBLIC_SUBGRAPH_URL as string;
 
 // Define types for the subgraph response
 interface SubgraphPlayer {
