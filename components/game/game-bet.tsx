7fdd2ba00368b3e32b3124330e13f3245e83d3c5
Nuno Carvalho <nuno@mygateway.xyz>
2025-03-07 02:35:47 +0000

    fix: Enhance responsive design for game bet component

--- components/game/game-bet.tsx
+++ components/game/game-bet.tsx
@@ -23,9 +23,9 @@ export default function GameBet({ onBet, value, errorMessage }: GameBetProps) {
       <div
         className={`border flex flex-col sm:flex-row px-4 py-4 mt-4 rounded-lg ${
           errorMessage ? "border-red-500" : "border-zinc-700"
-        } justify-between items-center gap-4`}
+        } justify-between items-start md:items-center gap-4`}
       >
-        <div className="flex flex-grow-1">
+        <div className="flex md:flex-grow-1">
           <input
             className="bg-transparent border-transparent  outline-none text-white focus:transparent focus:border-transparent text-md font-normal leading-none text-center uppercase flex-grow-0 w-32 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
             type="number"
@@ -37,18 +37,18 @@ export default function GameBet({ onBet, value, errorMessage }: GameBetProps) {
             MON
           </div>
         </div>
-        <div className="flex flex-wrap gap-4">
+        <div className="flex gap-4 max-sm:w-full">
           {[0.1, 0.25, 0.5, 1].map((amount) => (
             <button
               key={amount}
               onClick={() => onBet(amount)}
-              className={`bg-zinc-950 self-stretch flex min-w-16 items-center overflow-hidden justify-center my-auto opacity-80 px-4 py-1.5 border rounded-md border-solid transition-all duration-300  ${
+              className={`bg-zinc-950 self-stretch flex items-center overflow-hidden justify-center w-full opacity-80 px-4 py-1.5 border rounded-md border-solid transition-all duration-300  ${
                 value === amount
                   ? "border-[rgba(141,12,255,1)] group-hover:opacity-50 hover:!opacity-100"
                   : "border-zinc-700 hover:border-[rgba(141,12,255,1)] group-hover:opacity-50 hover:!opacity-100"
               }`}
             >
-              {amount} MON
+              {amount} <span className="hidden sm:block"> MON</span>
             </button>
           ))}
         </div>
