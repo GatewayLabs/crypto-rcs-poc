781d086cc666d6ac760c380ac2edd8b23b306e7a
Lucas (Kbooz) <lucas1inacio7souza@gmail.com>
2025-03-10 16:04:59 -0300

    feat: hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import { Analytics } from "@vercel/analytics/next";
 import { GoogleTagManager } from "@next/third-parties/google"
 import "./globals.css";
+import Script from "next/script";
 
 const Providers = dynamic(() => import("./providers"), {
   ssr: false,
@@ -25,6 +26,16 @@ export default function RootLayout({
         <Providers>{children}</Providers>
         <Analytics />
         <GoogleTagManager gtmId="G-X0FKGJF5VW" />
+        <Script id="hotjar">
+          {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5328842,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
       </body>
     </html>
   );
