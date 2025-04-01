8ea2e8e39c9aa02e36eba45b6052d40c2fbab9e1
Nuno Carvalho <nunoormaker21@gmail.com>
2025-04-01 14:46:48 -0300

    chore: change bet values to 0.1, 0.5, 1 and 5 MON

--- components/game/game-bet.tsx
+++ components/game/game-bet.tsx
@@ -1,5 +1,5 @@
-import { useGameUIStore } from "@/stores/game-ui-store";
-import { Dispatch, SetStateAction, useEffect } from "react";
+import { useGameUIStore } from '@/stores/game-ui-store';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 
 interface GameBetProps {
   onBet: Dispatch<SetStateAction<number>>;
@@ -22,7 +22,7 @@ export default function GameBet({ onBet, value, errorMessage }: GameBetProps) {
     <>
       <div
         className={`border flex flex-col sm:flex-row px-4 py-4 mt-4 rounded-lg ${
-          errorMessage ? "border-red-500" : "border-zinc-700"
+          errorMessage ? 'border-red-500' : 'border-zinc-700'
         } justify-between items-start md:items-center gap-4`}
       >
         <div className="flex md:flex-grow-1">
@@ -38,14 +38,14 @@ export default function GameBet({ onBet, value, errorMessage }: GameBetProps) {
           </div>
         </div>
         <div className="flex gap-4 max-sm:w-full">
-          {[0.1, 0.25, 0.5, 1].map((amount) => (
+          {[0.1, 0.5, 1, 5].map((amount) => (
             <button
               key={amount}
               onClick={() => onBet(amount)}
               className={`bg-zinc-950 self-stretch flex items-center overflow-hidden justify-center w-full opacity-80 px-4 py-1.5 border rounded-md border-solid transition-all duration-300  ${
                 value === amount
-                  ? "border-[rgba(141,12,255,1)] group-hover:opacity-50 hover:!opacity-100"
-                  : "border-zinc-700 hover:border-[rgba(141,12,255,1)] group-hover:opacity-50 hover:!opacity-100"
+                  ? 'border-[rgba(141,12,255,1)] group-hover:opacity-50 hover:!opacity-100'
+                  : 'border-zinc-700 hover:border-[rgba(141,12,255,1)] group-hover:opacity-50 hover:!opacity-100'
               }`}
             >
               {amount} <span className="hidden sm:block"> MON</span>
